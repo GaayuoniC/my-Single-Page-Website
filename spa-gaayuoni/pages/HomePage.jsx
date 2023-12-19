b8b47ef3eb4118ebbb1c86999cf350e377b974ff
Christian Gaayuoni <104912138+ChrisGaay@users.noreply.github.com>
2023-12-19 20:34:48 +0100

    initial commit

---
+++ spa-gaayuoni/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+
+
+export function HomePage(){
+
+
+
+    return <>
+    <h1>Chris. G</h1>
+    
+    
+    
+    </>
+}
---
+++ spa-gaayuoni/src/App.jsx
@@ -0,0 +1,18 @@
+import { HomePage } from '../pages/HomePage'
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <HomePage/>
+  
+      
+    </>
+  )
+}
+
+export default App
