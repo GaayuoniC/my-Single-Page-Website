90bf2e0c114e314d5cb799bd6ace8b420f21a970
Christian Gaayuoni <104912138+ChrisGaay@users.noreply.github.com>
2024-01-02 13:26:38 +0100

    almost complete

--- spa-gaayuoni/pages/HomePage.jsx
+++ spa-gaayuoni/pages/HomePage.jsx
@@ -5,10 +5,11 @@ import { Header } from "../components/Header"
 
 export function HomePage(){
 
-   return(<><h1>Christian Gaayuoni.</h1>
+   return(<div className="home-container">
+    <h1>Christian Gaayuoni.</h1>
    <hr/>
    <h2>Frontend Developer <br/> living in Germany.</h2>
-   </>)
+   </div>)
       
       
      
