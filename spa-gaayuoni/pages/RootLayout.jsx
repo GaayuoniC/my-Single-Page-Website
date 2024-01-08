d6a62e24ec2468f84ed9a4e87dc86f82a79cfe8e
Christian Gaayuoni <104912138+ChrisGaay@users.noreply.github.com>
2024-01-09 00:52:34 +0100

    Working on responsiveness and burger menu

--- spa-gaayuoni/pages/RootLayout.jsx
+++ spa-gaayuoni/pages/RootLayout.jsx
@@ -1,36 +1,60 @@
-import { Life } from "../pages/Life"
-import {Cv} from "../pages/Cv"
-import {Contact} from "../pages/Contact"
-import { HomePage } from "./HomePage"
+import { Life } from "../pages/Life";
+import { Cv } from "../pages/Cv";
+import { Contact } from "../pages/Contact";
+import { HomePage } from "./HomePage";
+import "../pages/RootLayout.css";
 
-import { useState } from "react"
+import { useState } from "react";
 
-export function RootLayout(){
+export function RootLayout() {
+  const [currentPage, setCurrentPage] = useState("");
 
-  const [currentPage, setCurrentPage]= useState('')
+  const MENU = [
+    {
+      title: "Home",
+      link: "#",
+    },
+    {
+      title: "Life",
+      link: "#",
+    },
+    {
+      title: "CV",
+      link: "#",
+    },
+    {
+      title: "Contact",
+      link: "#",
+    },
+  ];
 
-  function handleSelectCurrentPage(text){
-    setCurrentPage(text)
+  function handleSelectCurrentPage(text) {
+    setCurrentPage(text);
   }
 
+  return (
+    <>
+      <nav>
+        <div className="logo"> Christian Gaayuoni</div>
 
+        <ul className="desktop-menu">
+          <li onClick={() => handleSelectCurrentPage("home")}>Home</li>
+          <li onClick={() => handleSelectCurrentPage("life")}>Life</li>
+          <li onClick={() => handleSelectCurrentPage("cv")}>CV</li>
+          <li onClick={() => handleSelectCurrentPage("contact")}>Contact</li>
+        </ul>
+        <ul className="mobile-menu">
+          <li onClick={() => handleSelectCurrentPage("home")}>Home</li>
+          <li onClick={() => handleSelectCurrentPage("life")}>Life</li>
+          <li onClick={() => handleSelectCurrentPage("cv")}>CV</li>
+          <li onClick={() => handleSelectCurrentPage("contact")}>Contact</li>
+        </ul>
+      </nav>
 
-  return   (<>
- 
- <ul className="header-container" >
-   
-      <li onClick={()=> handleSelectCurrentPage('home')} >Home</li>
-      <li onClick={()=>handleSelectCurrentPage('life')}>Life</li> 
-        <li onClick={()=>handleSelectCurrentPage('cv')}>CV</li>
-      <li onClick={()=>handleSelectCurrentPage('contact')}>Contact</li> 
-  </ul>
-
-  
- {currentPage === 'cv' && <Cv/>}
-  {currentPage === 'contact' && <Contact/>}  
-  {
-    currentPage === 'life' && <Life/>
-  }
-  {currentPage === 'home' && <HomePage/> }</>)
-    
+      {currentPage === "cv" && <Cv />}
+      {currentPage === "contact" && <Contact />}
+      {currentPage === "life" && <Life />}
+      {currentPage === "home" && <HomePage />}
+    </>
+  );
 }
