fc0babf36a022958365f17330cfd28c3f235caa5
Christian Gaayuoni <104912138+ChrisGaay@users.noreply.github.com>
2024-01-08 15:43:39 +0100

    add main picture and style

--- spa-gaayuoni/pages/HomePage.jsx
+++ spa-gaayuoni/pages/HomePage.jsx
@@ -1,23 +1,20 @@
 
 import { Header } from "../components/Header"
+import christian from "../src/images/christian.jpeg"
+import './HomePage.css'
 
+export function HomePage()
 
-
-export function HomePage(){
-
-   return(<div className="home-container">
+   {return(<div className="home-container">
     <h1>Christian Gaayuoni.</h1>
    <hr/>
    <h2>Frontend Developer <br/> living in Germany.</h2>
+   <div>
+      <img id="main-christian-page" src= {christian} alt="christian"/>
+   </div>
    </div>)
       
       
-     
-
-
-
-    
-    
     
    
 }
