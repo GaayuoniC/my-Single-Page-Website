0847e92fb2cddda4ea76d156e9d14be091c03f8a
Christian Gaayuoni <104912138+ChrisGaay@users.noreply.github.com>
2024-01-09 01:59:15 +0100

    burger menu complete

--- spa-gaayuoni/pages/RootLayout.jsx
+++ spa-gaayuoni/pages/RootLayout.jsx
@@ -2,40 +2,41 @@ import { Life } from "../pages/Life";
 import { Cv } from "../pages/Cv";
 import { Contact } from "../pages/Contact";
 import { HomePage } from "./HomePage";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import "../pages/RootLayout.css";
 
 import { useState } from "react";
 
 export function RootLayout() {
   const [currentPage, setCurrentPage] = useState("");
-
-  const MENU = [
-    {
-      title: "Home",
-      link: "#",
-    },
-    {
-      title: "Life",
-      link: "#",
-    },
-    {
-      title: "CV",
-      link: "#",
-    },
-    {
-      title: "Contact",
-      link: "#",
-    },
-  ];
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   function handleSelectCurrentPage(text) {
     setCurrentPage(text);
+  }
+
+  function handleMenuIsDisplayed() {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   }
 
   return (
     <>
       <nav>
         <div className="logo"> Christian Gaayuoni</div>
+        {isMobileMenuOpen ? (
+          <FontAwesomeIcon
+            className="mobile-menu-button"
+            icon={faXmark}
+            onClick={handleMenuIsDisplayed}
+          />
+        ) : (
+          <FontAwesomeIcon
+            className="mobile-menu-button"
+            icon={faBars}
+            onClick={handleMenuIsDisplayed}
+          />
+        )}
 
         <ul className="desktop-menu">
           <li onClick={() => handleSelectCurrentPage("home")}>Home</li>
@@ -43,12 +44,16 @@ export function RootLayout() {
           <li onClick={() => handleSelectCurrentPage("cv")}>CV</li>
           <li onClick={() => handleSelectCurrentPage("contact")}>Contact</li>
         </ul>
-        <ul className="mobile-menu">
-          <li onClick={() => handleSelectCurrentPage("home")}>Home</li>
-          <li onClick={() => handleSelectCurrentPage("life")}>Life</li>
-          <li onClick={() => handleSelectCurrentPage("cv")}>CV</li>
-          <li onClick={() => handleSelectCurrentPage("contact")}>Contact</li>
-        </ul>
+        {isMobileMenuOpen ? (
+          <ul className="mobile-menu">
+            <li onClick={() => handleSelectCurrentPage("home")}>Home</li>
+            <li onClick={() => handleSelectCurrentPage("life")}>Life</li>
+            <li onClick={() => handleSelectCurrentPage("cv")}>CV</li>
+            <li onClick={() => handleSelectCurrentPage("contact")}>Contact</li>
+          </ul>
+        ) : (
+          ""
+        )}
       </nav>
 
       {currentPage === "cv" && <Cv />}
