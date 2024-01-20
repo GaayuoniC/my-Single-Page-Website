2ab42372d823368097812e0700f24fe72b50ec2d
Christian Gaayuoni <104912138+ChrisGaay@users.noreply.github.com>
2024-01-20 21:44:27 +0100

    back to basics of javascript

--- spa-gaayuoni/pages/HomePage.jsx
+++ spa-gaayuoni/pages/HomePage.jsx
@@ -5,11 +5,35 @@ import "./HomePage.css";
 export function HomePage() {
   return (
     <div className="home-container">
-      <h2>
-        Frontend Developer <br /> living in Germany.
-      </h2>
-      <div>
+      <div className="container-item-1">
+        <h2>
+          Frontend Developer <br /> living in Germany
+        </h2>
+      </div>
+      <div className="container-item-2">
         <img id="main-christian-page" src={christian} alt="christian" />
+      </div>
+      <div className="container-item-3">
+        <p>
+          Welcome!
+          <br /> My name is Christian and am originally from Ghana. Having spent
+          the first half of my life pursuing two of my dreams in life, I have
+          now gotten to the point where I finally get the opportunity to achieve
+          my last dream, that is finally venturing into the world of IT. So here
+          I am, starting a carrier change and venturing into the world of
+          software development in Germany.
+        </p>
+        <p>
+          This is just the beginning of the development of my own website which
+          I started developing right from the first day that I started my
+          <strong> Frontend Web and App </strong> developement Bootcamp with{" "}
+          <strong>
+            <a href="https://talents.taktsoft.com/" target="_blank">
+              Taktsoft Campust Talents {""}
+            </a>
+          </strong>
+          in Bonn, Germany.
+        </p>
       </div>
     </div>
   );
