a19797658b519350eae9683ef3f58e2fe2092989
Christian Gaayuoni <104912138+ChrisGaay@users.noreply.github.com>
2024-01-22 08:53:30 +0100

    updating info for my pages and styles

--- spa-gaayuoni/pages/HomePage.jsx
+++ spa-gaayuoni/pages/HomePage.jsx
@@ -1,20 +1,18 @@
+import { Header } from "../components/Header";
+import christian from "../src/images/christian.jpeg";
+import "./HomePage.css";
 
-import { Header } from "../components/Header"
-import christian from "../src/images/christian.jpeg"
-import './HomePage.css'
-
-export function HomePage()
-
-   {return(<div className="home-container">
-    <h1>Christian Gaayuoni.</h1>
-   <hr/>
-   <h2>Frontend Developer <br/> living in Germany.</h2>
-   <div>
-      <img id="main-christian-page" src= {christian} alt="christian"/>
-   </div>
-   </div>)
-      
-      
-    
-   
+export function HomePage() {
+  return (
+    <div className="home-container">
+      <h1>Christian Gaayuoni</h1>
+      <hr />
+      <h2>
+        Frontend Developer <br /> living in Germany
+      </h2>
+      <div>
+        <img id="main-christian-page" src={christian} alt="christian" />
+      </div>
+    </div>
+  );
 }
--- spa-gaayuoni/pages/Life.jsx
+++ spa-gaayuoni/pages/Life.jsx
@@ -1,6 +1,56 @@
+import "./Life.css";
+export function Life() {
+  return (
+    <>
+      <h2>Living life</h2>
 
-export function Life(){
-    return (<>
-    <h2>Living life</h2>
-    
- </>)}
+      <div className="myLife-container">
+        <div className="myLife-item">
+          <h3>Early childhood</h3>
+          <section>
+            <p>
+              Born in Cape Coast, a city along the coast of Ghana in West
+              Africa. The first child of five siblings. Raised in Tamale in the
+              northern region of Ghana. Father was a teacher and mother was a
+              nurse. I started my basic education in Tamale, the Northern
+              regional capital in the north of Ghana.
+            </p>
+            <p></p>
+          </section>
+        </div>
+        <div className="myLife-item">
+          <h3>Teen years</h3>
+
+          <p>
+            I attended Tamale Secondary School from 1989 to 1994 and obtained my
+            General Certificate of Education, Ordinary Level (GCE O Level).
+            After graduation I gained admission to Prebyterian Boys Secondary
+            School in Accra, the capital city of Ghana. The school was
+            considered one of the best schools in Ghana. After two years of
+            studies in Geography, Economics and Government, I passed my Advanced
+            Level certification, the equivalent of level 100 in the United
+            Kingdom ie first year of university education.
+          </p>
+          <p>
+            After completing my Advanced Level education it was mandatory for
+            for me to do national service. I was posted as a teacher to Vittin
+            Secondary School in Tamale where I taught economics. This was
+            1996/97 academic year. During my service I was able to apply for a
+            job as a Radio Presenter with the Ghana Broadcasting Corporation in
+            Tamale.
+          </p>
+          <p>
+            I worked as a Radio Presenter and a DJ from 1997 to 1998. During
+            this time I introduced more lively programmes and attracted more
+            listeners to the radio station. The name of the radio station was
+            and still is Radio Savanna FM and my radio name was Kriss Gee.
+          </p>
+        </div>
+        <div className="myLife-item">
+          <h3>Adulthood</h3>
+          <p></p>
+        </div>
+      </div>
+    </>
+  );
+}
