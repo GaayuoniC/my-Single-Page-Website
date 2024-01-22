18038c55da5ebecc6e1b1e8e6a752a71ee9430bc
Christian Gaayuoni <104912138+ChrisGaay@users.noreply.github.com>
2024-01-22 09:28:15 +0100

    modified the layout of the spa

--- spa-gaayuoni/src/App.jsx
+++ spa-gaayuoni/src/App.jsx
@@ -1,17 +1,39 @@
-import { RootLayout } from '../pages/RootLayout'
-import { useState } from 'react'
+import { Life } from "../pages/Life";
+import { Cv } from "../pages/Cv";
+import { Contact } from "../pages/Contact";
+import { HomePage } from "../Pages/HomePage";
 
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  //set the useState initial value to the 'home' page
+  const [currentPage, setCurrentPage] = useState("home");
+
+  //conditional rending for displaying pages
+  function handleSelectCurrentPage(text) {
+    setCurrentPage(text);
+  }
 
   return (
     <>
-    <RootLayout/>
-  
-      
+      <ul className="header-container">
+        {/* using onclick here along with the callback function for rending the 
+        the various pages */}
+        <li onClick={() => handleSelectCurrentPage("home")}>Home</li>
+        <li onClick={() => handleSelectCurrentPage("life")}>Life</li>
+        <li onClick={() => handleSelectCurrentPage("cv")}>CV</li>
+        <li onClick={() => handleSelectCurrentPage("contact")}>Contact</li>
+      </ul>
+      {currentPage === "home" && <HomePage />}
+      {currentPage === "cv" && <Cv />}
+      {currentPage === "contact" && <Contact />}
+      {currentPage === "life" && <Life />}
+
+      {/* <RootLayout/> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
